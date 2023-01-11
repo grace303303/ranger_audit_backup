22c7de2ca7e5b09edbea3a6cad30f307502227f1
gracezhucl <gracezhu@cloudera.com>
2023-01-10 21:06:25 -0500

    not work API with jaas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,29 @@
 package org.example;
 
 
+import okhttp3.Response;
+import org.json.simple.parser.ParseException;
+
+import javax.security.auth.login.LoginException;
 import java.io.IOException;
+import java.security.PrivilegedActionException;
 
-
+import static org.example.UpdateSolr.callWithKerberos;
 import static org.example.Utilities.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
 
+    final String localDir = "tmp_logs";
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException, PrivilegedActionException, LoginException {
+//        String data = readFileAsJsonList("test.log");
+
+        String data = "[{\"repoType\":1,\"repo\":\"cm_hdfs\",\"reqUser\":\"hbase\",\"evtTime\":\"2022-12-07 16:37:32.806\",\"access\":\"delete\",\"resource\":\"/hbase/.tmp\",\"resType\":\"path\",\"action\":\"write\",\"result\":1,\"agent\":\"hdfs\",\"policy\":-1,\"reason\":\"/hbase/.tmp\",\"enforcer\":\"hadoop-acl\",\"cliIP\":\"10.117.226.79\",\"reqData\":\"delete/CLI\",\"agentHost\":\"gracezhugracezhutesttest\",\"logType\":\"RangerAudit\",\"id\":\"dc35b082-8599-40c2-8726-0720f7bf640b-0\",\"seq_num\":1,\"event_count\":1,\"event_dur_ms\":1,\"tags\":[],\"additional_info\":\"{\\\"forwarded-ip-addresses\\\":\\\"[]\\\",\\\"remote-ip-address\\\":\\\"10.117.226.79\\\",\\\"accessTypes\\\":\\\"[read, write, execute]\\\"}\",\"cluster_name\":\"gracezhu-aws-env6\"}]";
+        callWithKerberos(data);
+//
+//        AzureClient azureClient = new AzureClient("data@gracezhuresourcegroupsan.dfs.core.windows.net",
+//                "");
+//
+//        azureClient.downloadFromCloud("tmp_logs", 2);
 
 
     }
--- src/main/java/org/example/UpdateSolr.java
+++ src/main/java/org/example/UpdateSolr.java
@@ -1,35 +1,64 @@
 package org.example;
 
+import com.sun.security.auth.callback.TextCallbackHandler;
 import okhttp3.*;
 import org.apache.hadoop.security.UserGroupInformation;
 
 import javax.net.ssl.*;
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
 import java.io.IOException;
+import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.security.cert.CertificateException;
 
+import static javax.security.auth.Subject.doAs;
+
 
 public class UpdateSolr {
-    public static void callAPI(String data) throws IOException, InterruptedException {
+    public static void callWithKerberos(String data) throws IOException, LoginException, PrivilegedActionException, InterruptedException {
 
-        UserGroupInformation.loginUserFromKeytab("solr/gracezhu-aws-env-master0.gracezhu.xcu2-8y8x.dev.cldr.work@GRACEZHU.XCU2-8Y8X.DEV.CLDR.WORK", "/run/cloudera-scm-agent/process/1546336269-solr-SOLR_SERVER/solr.keytab");
-        UserGroupInformation ugi = UserGroupInformation.getCurrentUser();
-        ugi.doAs((PrivilegedExceptionAction<Object>) () -> {
+        System.setProperty("java.security.auth.login.config", "../../run/cloudera-scm-agent/process/1546335586-solr-SOLR_SERVER/jaas.conf");
+        LoginContext lc = new LoginContext("Client", new TextCallbackHandler());
+        lc.login();
+        Subject subject = lc.getSubject();
+        System.out.println("Subject: " + subject);
 
-            OkHttpClient client = getUnsafeOkHttpClient();
-            MediaType mediaType = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(data, mediaType);
-            Request request = new Request.Builder()
-                    .url("https://gracezhu-aws-env-gateway.gracezhu.xcu2-8y8x.dev.cldr.work/gracezhu-aws-env/cdp-proxy/solr/ranger_audits/update?commitWithin=1000&overwrite=true&wt=json")
-                    .post(body)
-                    .addHeader("Content-Type", "application/json")
-                    .build();
-            Response response = client.newCall(request).execute();
-
-            System.out.println(response);
-
-            return response;
+        Object returnObject = Subject.doAs(subject, new PrivilegedExceptionAction<Object>() {
+            @Override
+            public Object run() throws Exception {
+                callAPI(data);
+                return null;
+            }
         });
+
+//        UserGroupInformation.loginUserFromKeytab("solr/gracezhu-aws-env-longrunning-master0.gracezhu.xcu2-8y8x.dev.cldr.work@GRACEZHU.XCU2-8Y8X.DEV.CLDR.WORK", "/var/run/cloudera-scm-agent/process/1546335586-solr-SOLR_SERVER/solr.keytab");
+//        UserGroupInformation ugi = UserGroupInformation.getCurrentUser();
+//
+//        ugi.doAs(new PrivilegedExceptionAction<Void>() {
+//            public Void run() throws Exception {
+//                callAPI(data);
+//                return null;
+//            }
+//        });
+
+    }
+
+    private static Response callAPI(String data) throws IOException {
+
+        OkHttpClient client = getUnsafeOkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(data, mediaType);
+        Request request = new Request.Builder()
+                .url("https://gracezhu-aws-env-longrunning-master0.gracezhu.xcu2-8y8x.dev.cldr.work:8985/solr/ranger_audits/update?commitWithin=1000&overwrite=true&wt=json")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response);
+
+        return response;
 
     }
 
