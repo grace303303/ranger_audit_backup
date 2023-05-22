c188bccfa8d364e268c60619777a7280f413c708
gracezhucl <gracezhu303@gmail.com>
2023-05-22 13:14:24 -0400

    checkStyle for CloudClient

--- src/main/java/org/rangeraudit/CloudClient.java
+++ src/main/java/org/rangeraudit/CloudClient.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface CloudClient {
+    /**
+     * The interface for Cloud clients.
+     */
     ArrayList<String> getAllValidLogPaths(int daysAgo);
     File downloadFromCloud(String s3logPath, String localDir) throws IOException;
 }
